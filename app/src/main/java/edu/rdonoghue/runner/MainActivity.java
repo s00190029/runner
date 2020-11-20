da8ef817c474373d2cacb304fee709a9c38545f1
s00190029 <mrdaylight@protonmail.com>
2020-11-20 23:21:30 +0000

    init sensor

--- app/src/main/java/edu/rdonoghue/runner/MainActivity.java
+++ app/src/main/java/edu/rdonoghue/runner/MainActivity.java
@@ -2,14 +2,24 @@ package edu.rdonoghue.runner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    TextView time, steps;
+    Button btnShowRun;
     CountUpTimer timer;
-    TextView counter;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    int internalSteps;
 
 
     @Override
@@ -17,14 +27,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        counter = findViewById(R.id.tvCounter);
+        // var delcare
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
+
+        time = findViewById(R.id.tvTimeNum);
         timer = new CountUpTimer(30000) {
             @Override
             public void onTick(int second) {
-                counter.setText(String.valueOf(second));
+                time.setText(String.valueOf(second));
             }
         };
+
     }
+
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this,mSensor);
+    }
+
 
     public void doStart(View view){
         timer.start();
@@ -37,8 +60,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doReset(View view){
-        counter.setText("0");
+        time.setText("0");
         Toast.makeText(this, "Timer reset", Toast.LENGTH_SHORT).show();
     }
 
+    public void doShowRun(View view) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
---
+++ app/src/main/java/edu/rdonoghue/runner/showRun.java
@@ -0,0 +1,14 @@
+package edu.rdonoghue.runner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showRun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_run);
+    }
+}
